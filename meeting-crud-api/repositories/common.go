6f4040f7357d04813e5258e4ac599ad39cd1fb23
Vituchon <victor@omniasalud.com>
2023-05-25 16:54:54 -0300

    amend: using animal as parameter name

--- meeting-crud-api/repositories/common.go
+++ meeting-crud-api/repositories/common.go
@@ -13,7 +13,7 @@ var InvalidEntityStateErr error = errors.New("Entity state is invalid")
 type Animals interface { // NOTAR queno llamo AnimalsRepository pues el nombre del paquete sirve de prefijo y al usar estar interfaz desde otro paquete queda `repositories.Games`, ver  @presentation\web\controllers\games.go#gamesRepository
 	GetAll() ([]model.Animal, error)
 	GetById(id int) (*model.Animal, error)
-	Create(game model.Animal) (created *model.Animal, err error)
-	Update(game model.Animal) (updated *model.Animal, err error)
+	Create(animal model.Animal) (created *model.Animal, err error)
+	Update(animal model.Animal) (updated *model.Animal, err error)
 	Delete(id int) error
 }
